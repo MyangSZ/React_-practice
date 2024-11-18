4250ee78fabbe7ca22aaf20300056810597ddbed
MyangSZ <unoyou@naver.com>
2024-11-19 02:05:48 +0900

    챕터4-3 React Router실습 동물조아page

---
+++ 4-3React-Router-practice/src/page/Main.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+import { data } from "../assets/data/data"
+//
+
+function Main() {
+    return <ul>
+        {data.map(el => <li key={el.id}>
+            <Link to={`/detail/${el.id}`}>
+            <div>{el.name}</div>
+            <img src={el.img} />
+            </Link>
+        </li>)}
+        </ul>
+}
+
+export default Main;
